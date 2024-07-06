02604e235423c54ddca3bfcfb3d8bd11e7591bb9
TheRiverElder <765375399@qq.com>
2024-07-07 01:59:31 +0800

    +MoveComponent

---
+++ src/buildin/components/MoveComponent.ts
@@ -0,0 +1,39 @@
+import Entity from "../entities/Entity";
+import Site from "../Site";
+import ComponentBase from "./CompoenentBase";
+
+export default class MoveComponent extends ComponentBase {
+
+    public static readonly ID = "move";
+
+    override get id(): string {
+        return MoveComponent.ID;
+    }
+
+    private _previousSite: Site | null = null;
+    public get previousSite(): Site | null {
+        return this._previousSite;
+    }
+
+    /**
+     * 走到新的地点
+     * @param newSite 要去的新的地点
+     * @param silient 如果为true，则不会地点改变的提示文字
+     */
+    goToSite(newSite: Site, silient: boolean = false): void {
+        const host = this.host;
+        if (!(host instanceof Entity)) throw new Error("Move component can only used on Entity");
+
+        this._previousSite = host.site;
+        host.site.removeEntity(host);
+
+        if (silient) {
+            newSite.entities.add(host);
+        } else {
+            newSite.addEntity(host);
+        }
+        
+        host.site = newSite;
+    }
+
+}
--- src/buildin/entities/Entity.ts
+++ src/buildin/entities/Entity.ts
@@ -14,25 +14,13 @@ abstract class Entity extends ObjectBase {
         if (!this._site) throw new Error("Site not set!");
         return this._site;
     }
+    public set site(site: Site) {
+        this._site = site;
+    }
 
     constructor(data: EntityData) {
         super(data);
         this._site = data.site ?? null;
-    }
-
-    /**
-     * 走到新的地点
-     * @param newSite 要去的新的地点
-     * @param silient 如果为true，则不会地点改变的提示文字
-     */
-    goToSite(newSite: Site, silient: boolean = false): void {
-        this.site.removeEntity(this);
-        if (silient) {
-            newSite.entities.add(this);
-        } else {
-            newSite.addEntity(this);
-        }
-        this._site = newSite;
     }
 
     /**
